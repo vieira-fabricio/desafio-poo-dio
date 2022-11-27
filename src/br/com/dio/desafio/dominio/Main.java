d34d6b86915379a7d8b9e1f448f8b3c106cc72f9
fabrivbomfin <bvieira.fabricio@gmail.com>
2022-11-27 21:59:02 -0200

    Criando as classes curso e mentoria

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,30 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Curso curso1 = new Curso();
+		curso1.setTitulo("Curso Java");
+		curso1.setDescricao("Descrição Curso Java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo("Curso Js");
+		curso2.setDescricao("Descrição Curso Js");
+		curso2.setCargaHoraria(4);
+		
+		Mentoria mentoriaJava = new Mentoria();
+		mentoriaJava.setTitulo("Mentoria Java");
+		mentoriaJava.setDescricao("Descrição Mentoria Java");
+		mentoriaJava.setData(LocalDate.now());
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoriaJava);
+		
+	}
+
+}
